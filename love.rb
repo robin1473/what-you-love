2779c19326cb06de4f76b48caa2c049365f94b4f
Jay <jpnappy@gmail.com>
2016-02-13 16:24:19 -0500

    first commit

---
+++ love.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+
+# define a function that accepts two parameters:
+	# name and what_you_love
+	# the function should be called captain_obvious 
+	# if I called the function with "Jay" and "Football" it should say:
+		 #My name is Jay and I love Football!
+
+
+
+# Show the user a question that says "what is your name?" 
+
+
+# Store the user's input in a variable 
+
+
+# Show the user a question that says "What do you love?"
+
+
+# Store the user's input in a variable 
+
+
+# Call the function 
