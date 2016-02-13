244754b474d0e9082b5ea583627bb1bb6b960881
bmasanga <b.masanga@gmail.com>
2016-02-13 16:45:15 -0500

    Ben's update

--- love.rb
+++ love.rb
@@ -7,18 +7,22 @@ require 'pry'
 	# if I called the function with "Jay" and "Football" it should say:
 		 #My name is Jay and I love Football!
 
+def captain_obvious(name, what_you_love)
+	puts("My name is #{name} and I love #{what_you_love}!")
+end
 
 
 # Show the user a question that says "what is your name?" 
-
+puts("What is your name?")
 
 # Store the user's input in a variable 
-
+user_name = gets.strip
 
 # Show the user a question that says "What do you love?"
-
+puts("What do you love?")
 
 # Store the user's input in a variable 
-
+user_love = gets.strip
 
 # Call the function 
+captain_obvious(user_name, user_love)
